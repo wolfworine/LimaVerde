780c3b5f36ac49c40b9bd4ea7e1a7e0924c82e5d
Cesar Valenzuela <cuvalenzuela@soaint.com>
2021-09-08 00:40:01 -0500

    eliminado

--- sp-mailing/src/main/java/pe/com/prima/spmailing/config/ApiDoc.java
+++
@@ -1,9 +0,0 @@
-package pe.com.prima.spmailing.config;
-
-public interface ApiDoc {
-
-    String RESPONSE_CODE_OK = "200";
-    String RESPONSE_CODE_BAD_REQUEST = "400";
-    String RESPONSE_CODE_NOTFOUND = "404";
-
-}
