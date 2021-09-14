1ea7fb76d00983175d3e5aeaceaf1d46c88b3d7f
Franclin Lopez <franclinlh@gmail.com>
2021-09-14 01:57:43 -0500

    modificar ruta de la base de datos y la configuración de cors

--- punto-reciclaje/src/main/java/pe/com/lima/verde/punto/reciclaje/PuntoReciclajeApplication.java
+++ punto-reciclaje/src/main/java/pe/com/lima/verde/punto/reciclaje/PuntoReciclajeApplication.java
@@ -3,11 +3,24 @@ package pe.com.lima.verde.punto.reciclaje;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
 public class PuntoReciclajeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PuntoReciclajeApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
--- registrar-usuario/src/main/java/pe/com/lima/verde/registrar/usuario/RegistrarUsuarioApplication.java
+++ registrar-usuario/src/main/java/pe/com/lima/verde/registrar/usuario/RegistrarUsuarioApplication.java
@@ -8,6 +8,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(description = "Componente de entidad registrar de Usuario.", title = "API Registrar Usuario", version = "1.0.0"), servers = @Server(url = "http://localhost:8092/"))
 @ComponentScan(basePackages = {"pe.com.lima.verde.registrar.usuario"})
@@ -15,6 +19,15 @@ public class RegistrarUsuarioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrarUsuarioApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
